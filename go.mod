f131fe88b213659c95e9ebae04d93f43f2ec3dcf
plantimals <5475390+plantimals@users.noreply.github.com>
2022-09-01 12:10:53 -0500

    fixed up knob to be a functional command line tool

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/plantimals/knob
 
 go 1.18
 
-require github.com/fiatjaf/go-nostr v0.7.4
+require (
+	github.com/fiatjaf/go-nostr v0.7.4
+	github.com/sirupsen/logrus v1.9.0
+)
 
 require (
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
@@ -11,4 +14,5 @@ require (
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/valyala/fastjson v1.6.3 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
--- knob.go
+++ knob.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
+	"strings"
 	"time"
 
 	nostr "github.com/fiatjaf/go-nostr"
+	log "github.com/sirupsen/logrus"
 )
 
 type RelayPolicy struct {
@@ -23,18 +27,49 @@ func (rp *RelayPolicy) ShouldWrite(e *nostr.Event) bool {
 	return rp.Write
 }
 
-func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: nostr <path>")
-		os.Exit(1)
-	}
-	path := os.Args[1]
-	f, err := ioutil.ReadFile(path)
+var genkeys, pause bool
+var path, relay, input string
+
+func initFlags() {
+	flag.BoolVar(&genkeys, "genkeys", false, "set to generate a new pub/priv key pair")
+	flag.BoolVar(&pause, "pause", false, "set to pause for 10 seconds to allow debugger to attach")
+	flag.StringVar(&relay, "relay", "wss://nostr.drss.io", "url of a relay to send to")
+	flag.StringVar(&path, "file", "", "file path to .md .txt or .json")
+	flag.StringVar(&input, "input", "", "input content")
+	flag.Parse()
+}
+
+func genkeysShow() (string, string) {
+	priv := nostr.GeneratePrivateKey()
+	pub, err := nostr.GetPublicKey(priv)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		panic(err)
+	}
+	fmt.Printf("{\"private_key\":\"%s\",\"public_key\":\"%s\"}\n", priv, pub)
+	return priv, pub
+}
+
+func main() {
+
+	initFlags()
+
+	//setup keys
+	var priv, pub string
+	var err error
+	if genkeys {
+		priv, pub = genkeysShow()
+		if input == "" {
+			os.Exit(0)
+		}
+	} else {
+		priv = os.Getenv("NOSTR_KEY")
+		pub, err = nostr.GetPublicKey(priv)
+		if err != nil {
+			panic(err)
+		}
 	}
 
+	//set up relay
 	pool := nostr.NewRelayPool()
 
 	p := &RelayPolicy{
@@ -42,33 +77,124 @@ func main() {
 		Write: true,
 	}
 
-	err = pool.Add("wss://nostr.drss.io", p)
+	err = pool.Add(relay, p)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	k := os.Getenv("NOSTR_KEY")
-	pool.SecretKey = &k
+	pool.SecretKey = &priv
 
-	event, statuses, _ := pool.PublishEvent(&nostr.Event{
+	//allow attachment of debugger
+	if pause {
+		time.Sleep(10 * time.Second)
+	}
+
+	events := make(chan *nostr.Event)
+	//process events
+	go func(events chan *nostr.Event) {
+		for evt := range events {
+			evt.PubKey = pub
+			err = evt.Sign(priv)
+			if err != nil {
+				panic(err)
+			}
+			ok, err := evt.CheckSignature()
+			if err != nil {
+				panic(err)
+			}
+			if !ok {
+				log.Errorf("event failed signature check")
+			} else {
+				log.Info("event passed signature check")
+			}
+
+			event, statuses, _ := pool.PublishEvent(evt)
+
+			wait := time.Tick(10 * time.Second)
+		forLoop:
+			for {
+				select {
+				case status := <-statuses:
+					switch status.Status {
+					case nostr.PublishStatusSent:
+						fmt.Printf("Sent event %s to '%s'.\n", event.ID, status.Relay)
+					case nostr.PublishStatusFailed:
+						fmt.Printf("Failed to send event %s to '%s'.\n", event.ID, status.Relay)
+					case nostr.PublishStatusSucceeded:
+						fmt.Printf("Event seen %s on '%s'.\n", event.ID, status.Relay)
+						break forLoop
+					}
+				case <-wait:
+					log.Errorf("timeout exceeded for event: %s", event.ID)
+					break forLoop
+				}
+			}
+
+			ShowEvent(event)
+		}
+
+	}(events)
+
+	if input != "" {
+		EventFromInput(input, pub, events)
+	} else if strings.HasSuffix(path, ".json") {
+		EventsFromJson(path, events)
+	} else if strings.HasSuffix(path, ".md") || strings.HasSuffix(path, ".txt") {
+		EventFromText(path, pub, events)
+	} else {
+		fmt.Println("no inputs found")
+	}
+	fmt.Println("finished")
+	close(events)
+	time.Sleep(1 * time.Second)
+}
+
+func ShowEvent(evt *nostr.Event) {
+	b, err := json.MarshalIndent(evt, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(b))
+}
+
+func EventFromText(path, pk string, events chan *nostr.Event) {
+	f, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	events <- &nostr.Event{
 		CreatedAt: time.Now(),
 		Kind:      nostr.KindTextNote,
 		Tags:      make(nostr.Tags, 0),
 		Content:   string(f),
-	})
+		PubKey:    pk,
+	}
+}
 
-	log.Printf("pubkey: %s", event.PubKey)
-	log.Printf("event:  %s", event.ID)
-	log.Printf("sig:    %s", event.Sig)
+func EventFromInput(input, pk string, events chan *nostr.Event) {
+	events <- &nostr.Event{
+		CreatedAt: time.Now(),
+		Kind:      nostr.KindTextNote,
+		Tags:      make(nostr.Tags, 0),
+		Content:   input,
+		PubKey:    pk,
+	}
+}
 
-	for status := range statuses {
-		switch status.Status {
-		case nostr.PublishStatusSent:
-			fmt.Printf("Sent event %s to '%s'.\n", event.ID, status.Relay)
-		case nostr.PublishStatusFailed:
-			fmt.Printf("Failed to send event %s to '%s'.\n", event.ID, status.Relay)
-		case nostr.PublishStatusSucceeded:
-			fmt.Printf("Event seen %s on '%s'.\n", event.ID, status.Relay)
+func EventsFromJson(path string, events chan *nostr.Event) {
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	buf := bufio.NewScanner(f)
+	for buf.Scan() {
+		var evt *nostr.Event
+		payload := []byte(buf.Text())
+		json.Unmarshal(payload, &evt)
+		if err != nil {
+			panic(err)
 		}
+		evt.CreatedAt = time.Now()
+		events <- evt
 	}
 }
